b59f18365498a37f664aa87fd6f8e1dae843e77a
Матвей Однодворцев <mr.onv78@mail.ru>
2023-12-12 18:18:56 +0300

    update header files

--- parse.h
+++ parse.h
@@ -2,17 +2,21 @@
 #define PARSE
 #include <stdbool.h>
 enum numSystem{
+    UNKNOWN = -1,
     BIN = 2,
     OCT = 8,
     HEX = 16
 };
 
+typedef long long LL;
+
 typedef struct Number{
     enum numSystem nSys;
-    int iNum;
+    LL iNum;
     char num[128];
     int sign;
 } Number;
+
 void printResult(Number n);
 void reverseStr(char *str);
 char getOperator(char *str);
@@ -21,8 +25,8 @@ Number parseNum(char *str);
 bool isValid(char *str, enum numSystem SYS);
 enum numSystem getSystem(char *num);
 int ctoi(char c);
-int stoi(char *str, enum numSystem Sys, int sign);
+LL stoi(char *str, enum numSystem Sys, int sign);
 char itoc(int i, enum numSystem Sys);
-char* itos(int i, enum numSystem Sys);
+char *itos(LL i, enum numSystem Sys, char *dest);
 
 #endif
