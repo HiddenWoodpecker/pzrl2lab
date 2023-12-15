f801e0ddfd66799e4022ad5e1fa36afb71d32206
Матвей Однодворцев <mr.onv78@mail.ru>
2023-12-15 21:27:04 +0300

    Добавлена обработка ошибки деления на ноль Улучшена читаемость файла

--- calculate.c
+++ calculate.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+
 LL calcUnoOperation(char operation, LL i1){
     if (operation != '~'){
         perror("Неизвестная унарная операция");
@@ -13,43 +14,36 @@ LL calcUnoOperation(char operation, LL i1){
 LL calcBinOperation(char operation, LL i1, LL i2){
     switch (operation){
         case '+':
-            
             return i1 + i2;
-            break;
         case '-':
             return i1 - i2;
-            break;
-        
         case '*':
             return i1 * i2;
-            break;
         case '%':
+            if (i2 == 0){
+                perror("Деление на ноль запрещено");
+                return -1;
+            }
             return i1 % i2;
-            break;
         case '&':
             if (i1 < 0 || i2 < 0){
                 perror("Операнды для побитовой операции должны быть неотрицательным");
                 return -1;
             }
             return i1 & i2;
-            break;
         case '|':
             if (i1 < 0 || i2 < 0){
                 perror("Операнды для побитовой операции должны быть неотрицательным");
                 return -1;
             }
             return i1 | i2;
-            break;
         case '^':
             if (i1 < 0 || i2 < 0){
                 perror("Операнды для побитовой операции должны быть неотрицательным");
                 return -1;
             }
             return i1 ^ i2;
-            break;
         default:
             return -1;
-            break;
         }
-    return -1;
 }
